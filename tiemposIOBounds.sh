79b2b2be3a10384d1e6367d240fe1e0b05dbae4b
kevmacal <kevmacal@espol.edu.ec>
2016-01-25 22:57:17 -0500

    Bash para mandar a ejecutar en grupo los archivos y retorna el tiempo de cada uno por consola

---
+++ tiemposIOBounds.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Aqui van los archivos a abrirse, separados por el espacio y en limit el numero de archivos a abrir
+nombresArchIn=("letras" "numeros" "Carchi.txt")
+limit=3
+
+#Toma el tiempo y al terminar muestra cuanto demoro en milliseconds
+for((i=0;$i<$limit;++i))
+do
+    T="$(date +%s%N)"
+    ./br -i ${nombresArchIn[$i]} -o output
+    T="$(($(date +%s%N)-T))"
+    M="$((T/1000000))"
+    echo "Archivo ${nombresArchIn[$i]} -- Tiempo en milisegundos: ${M}"
+done
+
+
